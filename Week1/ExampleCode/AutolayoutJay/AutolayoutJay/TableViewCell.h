36cf1559101ed0e7497c38086c235b91b5b89028
Jaykong <kongyunpeng2011@sina.com>
2015-11-17 12:48:13 +0800

    week 2 start

---
+++ Week1/ExampleCode/AutolayoutJay/AutolayoutJay/TableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.h
+//  AutolayoutJay
+//
+//  Created by trainer on 11/12/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titlelbl;
+
+@end
