e97286dcd8edb3c9aa7a2e1fa54a362535920ed8
Jaykong <kongyunpeng2011@sina.com>
2015-11-23 00:54:49 +0800

    第二周作业成绩

--- PanXiang/CourseWork2/PXWeek2/PXWeek2/ViewController.h
+++ PanXiang/CourseWork2/PXWeek2/PXWeek2/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UILabel *Number;
+@property (weak, nonatomic) IBOutlet UILabel *Number;//label的命名影响代码的可读性，更好命名应是numberLabel，对于属性值首字母习惯是命名为小写
 
 
 @end
--- PanXiang/CourseWork2/PXWeek22/PXWeek22/ViewController.m
+++ PanXiang/CourseWork2/PXWeek22/PXWeek22/ViewController.m
@@ -22,12 +22,13 @@
     arr = [NSArray arrayWithObjects:@"ntrinsic Content SizeThe intrinsic content size is the size a view prefers to have for a specific content it displays. For example, UILabel has a preferred height based on the font, and a preferred width based on the font and the text it displays. A UIProgressView only has a preferred height based on its artwork, but no preferred width. A plain UIView has neither a preferred width nor a preferred height.Compression Resistance and Content HuggingEach view has content compression resistance priorities and content hugging priorities assigned for both dimensions. These properties only take effect for views which define an intrinsic content size, otherwise there is no content size defined that could resist compression or be hugged.",@"Intrinsic Content SizeThe intrinsic content size is the size a view prefers to have for a specific content it displays. For example, UILabel has a preferred height based on the font, and a preferred width based on the font and the text it displays. A UIProgressView only has a preferred height based on its artwork, but no preferred width. A plain UIView has neither a preferred width nor a preferred height.Compression Resistance and Content HuggingEach view has content compression resistance priorities and content hugging priorities assigned for both dimensions. These properties only take effect for views which define an intrinsic content size, otherwise there is no content size defined that could resist compression or be hugged.",@"Intrinsic Content SizeThe intrinsic content size is the size a view prefers to have for a specific content it displays. For example, UILabel has a preferred height based on the font, and a preferred width based on the font and the text it displays. A UIProgressView only has a preferred height based on its artwork, but no preferred width. A plain UIView has neither a preferred width nor a preferred height.Compression Resistance and Content HuggingEach view has content compression resistance priorities and content hugging priorities assigned for both dimensions. These properties only take effect for views which define an intrinsic content size, otherwise there is no content size defined that could resist compression or be hugged.", nil];
     
     self.tableView.estimatedRowHeight = 500;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+   // self.tableView.rowHeight = UITableViewAutomaticDimension;
 }
+
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    self.tableView.estimatedRowHeight = 500;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    //self.tableView.estimatedRowHeight = 500;
+    //self.tableView.rowHeight = UITableViewAutomaticDimension;
     return UITableViewAutomaticDimension;
 }
 
